880fefba7726b59141defcb857737eb606033c21
Jason Amerine <amerine.19@gmail.com>
2017-09-26 10:37:56 -0400

    changes to online orders screen

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::ProductsController < ApplicationController
 
   def index
-    @products = Product.all
+    @products = Product.where(online_order_item: true)
+    @product_types = Product.product_types.keys
     @order_item = current_order.order_items.new
   end
 
